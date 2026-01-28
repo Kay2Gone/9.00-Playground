eba2440d59618bbeb83f6dff7897b2728a4fc7f8
Kay2Fried <74153922+KayBeen2Fried@users.noreply.github.com>
2026-01-27 18:38:02 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+window.addEventListener("load", (event) => {
+  currentUserAgent = navigator.userAgent;
+  allowedAgents = ["Safari", "Playstation 4", "9.00"];
+
+  if (isUserAgentAllowed(currentUserAgent)) {
+    alert("Pass");
+  } else {
+    alert("Not a Playstation. Tools will not work.");
+  }
+});
+
+function uaCheck() {
+  alert(navigator.userAgent);
+}
+
+function isUserAgentAllowed(userAgent) {
+  return allowedAgents.some((agent) => userAgent.includes(agent));
+}
+
+function parseJS() {
+  alert("parser will go here later");
+}
+
+function payloadListener() {
+  if (isUserAgentAllowed(currentUserAgent)) {
+    alert("Try sending payload to port 9020.");
+  } else {
+    alert("You must be on Playstation to use this tool.");
+  }
+}
