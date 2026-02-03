579eb1043e9817a2f7606008105d2de4b7131a40
Kay2Fried <74153922+Kay2Gone@users.noreply.github.com>
2026-02-03 10:03:20 -0600

    Implement user agent check on page load
    Add user agent check on window load and alert for compatibility.

--- js/payload.js
+++ js/payload.js
@@ -1,3 +1,22 @@
-function checkUA() {
+window.addEventListener("load", (event) => {
+ const currentUserAgent = navigator.userAgent;
+ const allowedAgents = ["Safari", "Playstation 4", "9.00"];
 
+  if (isUserAgentAllowed(currentUserAgent)) {
+//
+  } else {
+//
+  }
+});
+
+function checkUA() {
+if (isUserAgentAllowed(currentUserAgent)) {
+    alert("Compatible! Proceeding...");
+    startP();
+  }
+}
+
+function startP() {
+setTimeout(startP, 2000);
+alert("test");
 }
